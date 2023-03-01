940ebda86b9761bcef6af328b7d345acf75656e4
shehmaz1993 <shehmaz.islam@northsouth.edu>
2023-03-01 07:21:51 -0600

    ultimate modification

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -61,8 +61,8 @@ class _HomePageState extends State<HomePage> {
                 padding:  EdgeInsets.only(top:SizeConfig.blockSizeVertical*1.5,bottom: SizeConfig.blockSizeVertical*4),
                 child: IconButton(
                   onPressed: (){
-                    // Navigator.of(context).push(MaterialPageRoute(builder: (context) =>OrderRev()));
-                    //  Navigator. of(context). pop();
+                  //  Navigator.of(context).push(MaterialPageRoute(builder: (context) =>OrderRev()));
+                    // Navigator. of(context). pop();
                   },
                   icon:Padding(
                     padding: EdgeInsets.only(top:SizeConfig.blockSizeVertical*0.8),
