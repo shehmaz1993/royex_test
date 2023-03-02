12d2a676b226c66ebdbf9952821d3f2375d0c3e0
shehmaz1993 <shehmaz.islam@northsouth.edu>
2023-03-01 23:17:43 -0600

    ultimate design-1

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -2,24 +2,32 @@
 //import 'package:dio/dio.dart' as Dio;
 import 'dart:convert';
 
+import 'package:http/http.dart';
 import 'package:royex_test/Models/model_class.dart';
 import 'package:http/http.dart' as http;
 
 class Repository{
   String url='https://alhafidh-market-test.myshopify.com/admin/api/2023-01/products/7030430367788.json';
+
+  Future<Response> getResponse() async {
+    var response=  await http.get(
+      Uri.parse(url),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        "X-Shopify-Access-Token": "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",
+      },
+
+    );
+    return response;
+  }
+
+
+
   Future<List<ModelOfProduct>> fetchProducts()async{
     try{
-      var response = await http.get(
-        Uri.parse(url),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-          "X-Shopify-Access-Token": "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"
-        },
-
-      );
+      var response = await  getResponse();
       if(response.statusCode==200){
        var info = json.decode(response.body);
-       print(info.runtimeType);
        List<dynamic> lst=info['product']['images'];
        return lst.map((element) => ModelOfProduct.fromJson(element)).toList();
       }
@@ -33,19 +41,13 @@ class Repository{
   }
   Future<Map<String,dynamic>> fetchMap()async{
     try{
-      var response = await http.get(
-        Uri.parse(url),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-          "X-Shopify-Access-Token": "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"
-        },
+      var response =await  getResponse();
 
-      );
       if(response.statusCode==200){
         var info = json.decode(response.body);
-        print(info.runtimeType);
+
         Map<String,dynamic> map=info['product'];
-        print(map);
+
        return map;
       }
       else{
